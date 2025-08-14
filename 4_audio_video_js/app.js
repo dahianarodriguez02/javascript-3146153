6e295fd2425fa2ce2ea7229ca0c68b739cce664e
dahianarodriguez02 <murillolesly02@gmail.com>
2025-08-14 15:33:43 -0500

    audio_video_parte1

---
+++ 4_audio_video_js/app.js
@@ -0,0 +1,12 @@
+const video = document.querySelector('#video')
+const playVideo = document.querySelector('#play-video')
+const stopVideo = document.querySelector('#stop-video')  
+const playStopImg1 = document.querySelector('#pla-stop-yImg1')
+const playStopImg2 = document.querySelector('#play-stop-Img2')
+
+
+console.log(video)
+console.log(playVideo)
+console.log(stopVideo)
+console.log(playStopImg1)
+console.log(playStopImg2)
