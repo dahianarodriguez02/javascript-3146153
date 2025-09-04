abac7d04b2943e7fd4f673fa669d609e2cc88111
dahianarodriguez02 <murillolesly02@gmail.com>
2025-09-04 15:39:27 -0500

    Base y pagina GLITCHVERSE                  Nicol_Lesly_Alexa

---
+++ 6_base_datos_comic/bd.js
@@ -0,0 +1,52 @@
+const comic = {
+    "nombreComic": "Matrix",
+    "categoria": "apocalipsis",
+    "cantidadCap": 6,
+    "descripcion": "apocalipsis tecnologico",
+    "personjaes": ["Emma", "AlmaMamá", "Estudiante1", "Estudiante2","Estudiante3", "Noah", "AlmaTrabajo", "Dr.Rodriguez", "dR.Martinez", "Chappie", "Encapuchado", "Emily"],
+    "capitulos": [
+        {
+            "nombreCap": "Un día al revés",
+            "imgPortada": "./img/cap1.jpg",
+            "video": "video/cap1.mp4",
+            "descripcion": "Emma una joven de 19 años , comienza su primer día de universidad nerviosa mientras su madre la anima con ternura en la cocina. En el campues, se siente perdida, pero pronto conces a un grupo de estudiantes que la reciben con amabilidad. Al regresar a casa, comparte emocionada su experiencia con su madre, quien la escucha orgullosa.",
+            "personajes": ["Emma", "AlmaMamá", "Estudiante1", "Estudiante2"]
+        },
+        {
+            "nombreCap": "El inicio del fin",
+            "imgPortada": "./img/cap1.jpg",
+            "video": "video/cap1.mp4",
+            "descripcion": "Noah trabaja en su oficina cuando un apagón masivo deja la ciudad en caos: explosiones, aviones cayendo y gente desesperada. Junto a Alma huye entre gritos y disparos, esquivando multitudes y encapuchados armados.",
+            "personajes": ["Noah","AlmaTrabajo", "Encapuchado"]
+        },
+        {
+            "nombreCap": "La creación de Chappie",
+            "imgPortada": "./img/cap1.jpg",
+            "video": "video/cap1.mp4",
+            "descripcion": "En un laboratorio, los científicos crean a CHAPPIE, un robot con inteligencia avanzada. Al activarlo, la energía se descontrola y revela su propósito: destruir a la humanidad. El caos llega a Ámsterdam, ahora dominada por máquinas. Noah pierde a su familia y, junto a Alma, promete rebelarse. Mientras tanto, Emma vive el apagón en la universidad, donde la diversión se convierte en terror cuando aparecen robots asesinos.",
+            "personajes": ["Chappie","Dr.Rodriguez", "Dr.Martinez","Noah", "AlmaTrabajo", "Emma", "Estudiante1", "Estudiante2", "Estudiante3"]
+        },
+        {
+            "nombreCap": "El infierno",
+            "imgPortada": "./img/cap1.jpg",
+            "video": "video/cap1.mp4",
+            "descripcion": "Ámsterdam está en ruinas, dominada por robots. Noah descubre que su padre fue asesinado y que su madre y hermana desaparecieron; junto a Alma promete luchar por sobrevivir. En la universidad, Emma presencia un apagón durante un partido, y el terror comienza cuando sus compañeros son atacados y aparecen los ojos fríos de los robots.",
+            "personajes": ["Noah", "AlmaTrabajo", "Emma", "Estudiante1", "Estudiante2", "Estudiante3"]
+        },
+        {
+            "nombreCap": "El encuentro",
+            "imgPortada": "./img/cap1.jpg",
+            "video": "video/cap1.mp4",
+            "descripcion": "En una Ámsterdam en ruinas, Emma se cruza con Noah y Alma, perseguidos por un dron. Ella los ayuda, pero Alma muere tras revelar que los robots no fueron creados para destruir. Noah, devastado, entierra a su compañera con Emma. Entre la penumbra surge Chappie, un robot con señales de conciencia. Noah ve en él la última esperanza: cambiar su chip para que luche contra las máquinas. El cielo rojo anuncia que el fin ya ha comenzado.",
+            "personajes": ["Noah", "AlmaTrabajo", "Emma"]
+        },
+        {
+            "nombreCap": "Arreglar lo que no dañamos",
+            "imgPortada": "./img/cap1.jpg",
+            "video": "video/cap1.mp4",
+            "descripcion": "Emma y Noah logran insertar el chip en Chappie, restaurando su programación y devolviéndole la capacidad de controlar a los demás robots. Con su reinicio, la amenaza termina y el mundo empieza a recuperarse. Tres años después, Emma y Noah viven en paz en una granja junto a su hija Emily, mientras Chappie los acompaña, protegiendo y enseñando que la tecnología, bien usada, puede ser un aliado de la humanidad.",
+            "personajes": ["Noah", "Chappie", "Emma","Emily"]
+        },
+                       
+    ]
+}
